20abff23ea44540d631a0b68173aaf52eabee100
Bhawick Jain <bhawick@gmail.com>
2022-09-06 12:11:47 +0100

    create comment section CRUD functionality

---
+++ src/components/AddComment.tsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { useState } from "react";
+import BASE_URL from "./constants/BASE_URL";
+
+interface AddCommentProps {
+  pasteId: number;
+  setRefreshComments: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function AddComment({
+  pasteId,
+  setRefreshComments,
+}: AddCommentProps): JSX.Element {
+  const [commentMessage, setCommentMessage] = useState("");
+
+  const handleAddComment = () => {
+    console.log(`Adding Comment: ${commentMessage}`);
+    const AddCommentToPaste = async (pasteId: number) => {
+      try {
+        if (commentMessage.length > 0) {
+          await axios.post(`${BASE_URL}/pastes/${pasteId}/comments`, {
+            message: commentMessage,
+          });
+        } else {
+          console.log("Empty comment made");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    AddCommentToPaste(pasteId);
+    setCommentMessage("");
+    setRefreshComments((previousState) => !previousState);
+  };
+
+  return (
+    <p>
+      <input
+        onChange={(e) => setCommentMessage(e.target.value)}
+        value={commentMessage}
+        placeholder="add your thoughts!"
+      />
+      <button onClick={handleAddComment}>Add comment</button>
+    </p>
+  );
+}
