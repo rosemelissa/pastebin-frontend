629a3befd024b53de26134800867848197817fb4
Bhawick Jain <bhawick@gmail.com>
2022-09-08 11:28:55 +0100

    add react-router, implement router path handler to home and a paste unique ID

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,48 @@
 import { useState } from "react";
 import DisplayPastes from "./components/DisplayPastes";
 import PasteSubmit from "./components/PasteSubmit";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App(): JSX.Element {
-  const [refreshPastes, setRefreshPastes] = useState<boolean>(true);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<UniquePaste/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function UniquePaste(): JSX.Element {
+  const pathname = window.location.pathname
+  const regex = /\/(\d+)/g
+  const regexMatches = Array.from(pathname.matchAll(regex))[0]
+  const id = regexMatches[1]
+
   return (
     <>
-      <PasteSubmit setRefreshPastes={setRefreshPastes} />
-      <DisplayPastes
-        refreshPastes={refreshPastes}
-        setRefreshPastes={setRefreshPastes}
-      />
+        <p>You are seeing Paste with {id}</p>
     </>
-  );
+  )
+}
+function Home(): JSX.Element {
+
+  const [refreshPastes, setRefreshPastes] = useState<boolean>(true);
+
+    return (
+        <>
+          <PasteSubmit setRefreshPastes={setRefreshPastes} />
+          <DisplayPastes
+            refreshPastes={refreshPastes}
+            setRefreshPastes={setRefreshPastes}
+          />
+        </>
+    )
 }
 
 export default App;
